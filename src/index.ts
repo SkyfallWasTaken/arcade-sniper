fbd86b65e6ce1169cc3da381cee60f50f7830091
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-06 19:59:36 +0100

    More logging stuff

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import Cron from "croner";
 const ARCADE_USER_ID = process.env.ARCADE_USER_ID;
 const FILLOUT_SESSION_TOKEN = process.env.FILLOUT_SESSION_TOKEN;
 
-console.log("Launching browser...");
+console.log(`${chalk.green.bold("Launching")} browser`);
 const browser = await chromium.launch();
 const context = await browser.newContext();
 context.addCookies([
@@ -19,11 +19,14 @@ context.addCookies([
     domain: "forms.hackclub.com",
   },
 ]);
-console.log("Loading page...");
+
+console.log(`${chalk.green.bold("Loading")} page`);
 const page = await context.newPage();
+console.log(`${chalk.green.bold("Loaded")} page`);
 
 Cron("*/5 * * * *", async () => {
   const items = await getItems();
   console.log(`Fetched ${chalk.bold(items.length)} items.`);
   await executeContracts(items, page, ARCADE_USER_ID!, false);
 });
+console.log(`${chalk.green.bold("Running")} every 5 minutes`);
