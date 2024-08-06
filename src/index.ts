ed7e650a12be745bb95620303cf01fbc42ce9fe0
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-06 19:54:23 +0100

    Run every 5 minutes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { chromium } from "playwright";
 import executeContracts from "./contract";
 import { getItems } from "./arcadeShop";
 import chalk from "chalk";
+import Cron from "croner";
 
 const ARCADE_USER_ID = process.env.ARCADE_USER_ID;
 const FILLOUT_SESSION_TOKEN = process.env.FILLOUT_SESSION_TOKEN;
@@ -23,6 +24,8 @@ context.addCookies([
 console.log("Loading page...");
 const page = await context.newPage();
 
-const items = await getItems();
-console.log(`Fetched ${chalk.bold(items.length)} items.`);
-await executeContracts(items, page, ARCADE_USER_ID!, true);
+Cron("*/5 * * * *", async () => {
+  const items = await getItems();
+  console.log(`Fetched ${chalk.bold(items.length)} items.`);
+  await executeContracts(items, page, ARCADE_USER_ID!, true);
+});
